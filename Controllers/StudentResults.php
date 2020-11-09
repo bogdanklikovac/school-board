3e8c60bb1083490011ccac75fc9542ebafe2c76e
bogdan <klikovacbogdan@gmail.com>
2020-11-09 13:01:35 +0100

    Check criteria for  CSM students

--- Controllers/StudentResults.php
+++ Controllers/StudentResults.php
@@ -3,7 +3,59 @@
 
 class StudentResults
 {
+    const CSM = 'CSM';
+    const CSMB = 'CSMB';
+
     public function getStudentResult($id) {
-        return $id;
+        $student = Student::find($id);
+
+        $numOfStudentGrades = $student->grade()->count();
+
+
+        $studentBoard = $this->getSchoolBoard($id);
+        $studentInfo = Student::select('name','id')->where('id', $id)->get()->toArray();
+        $averageGrade = $this->getAverageGrades($id);
+
+        if($numOfStudentGrades >= 1 && $numOfStudentGrades <=4) {
+
+            if($studentBoard == self::CSM) {
+                //return Student::find($id)->toArray();
+                $grades = $this->getGrades($id);
+                $studentInfo[0]['averageGrade'] = $averageGrade;
+                $studentInfo[0]['grades'] = $grades;
+                $studentInfo[0]['status'] = ($averageGrade >= 7) ? 'PASS' : 'FAIL';
+                $result = json_encode($studentInfo);
+            }
+            return $result;
+        } else {
+            return "The student doesn't have the required number of grades.";
+        }
+
+
     }
+
+    public function getSchoolBoard($student_id){
+
+        $student = Student::find($student_id);
+        return $student->board->name;
+
+    }
+
+    public function getAverageGrades($id) {
+        $student = Student::find($id);
+        return $student->grade()->avg('grade');
+    }
+
+
+    public function getGrades($id) {
+        $grades = Grade::select('grade')->where('student_id', $id)->get()->toArray();
+        foreach ($grades as $key => $grade) {
+            unset($grades[$key]);
+            array_push($grades,$grade['grade']);
+        }
+
+        return array_values($grades);
+    }
+
+
 }
